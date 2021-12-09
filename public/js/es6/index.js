7e3bd090d3f5461f4bba85a45208e1192942d246
Alfred <ya95123@gmail.com>
2021-12-09 17:44:20 +0800

    dailog setting

--- public/js/es6/index.js
+++ public/js/es6/index.js
@@ -1,3 +1,16 @@
+const illustrateBtn = document.getElementById("illustrate-btn")
 const illustrate = document.getElementById("illustrate")
+const dailogClose = document.querySelector(".dailog-close")
 
-// TODO click 後打開說明事項
+// *btn 遊戲說明 - click 後打開說明事項
+illustrateBtn.addEventListener("click", () => {
+  illustrate.classList.remove("none")
+  illustrate.classList.remove("no-show")
+}, false)
+
+dailogClose.addEventListener("click", () => {
+  illustrate.classList.add("no-show")
+  setTimeout(() => {
+    illustrate.classList.add("none")
+  }, 150)
+})
