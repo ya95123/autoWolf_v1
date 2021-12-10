a56dcf228e81902a94d154f6820af50f49180c3f
Alfred <ya95123@gmail.com>
2021-12-10 18:30:30 +0800

    function select model / give chracter

--- public/js/es6/game.js
+++ public/js/es6/game.js
@@ -1,39 +1,53 @@
 // variables
 const setting = document.getElementById("setting")
+const giveCharacter = document.getElementById("giveCharacter")
 const models = document.querySelectorAll(".model")
 const modelData = {
   "cate": [
     {
       "model": "自訂模式",
       "peopleNum": "9-12",
-      "chracterAll": "",
-      "wolf": [],
-      "gods": [],
-      "mans": []
+      "wolfsNum": "",
+      "godsNum": "",
+      "mansNum": "",
+      "chracterText": "",
+      "characterAll": []
     },
     {
       "model": "一般局",
       "peopleNum": 9,
-      "chracterAll": "(3狼,預,女,獵,3民)",
-      "wolf": ["狼人", "狼人", "狼人"],
-      "gods": ["預言家", "女巫", "獵人"],
-      "mans": ["平民", "平民", "平民"]
+      "wolfsNum": 3,
+      "godsNum": 3,
+      "mansNum": 3,
+      "chracterText": "(3狼,預,女,獵,3民)",
+      "characterAll": ["狼人", "狼人", "狼人", "預言家", "女巫", "獵人", "平民", "平民", "平民"]
     },
     {
       "model": "狼王局",
       "peopleNum": 10,
-      "chracterAll": "(狼王,2狼,預,女,獵,騎,3民)",
-      "wolf": ["狼人", "狼人", "狼人"],
-      "gods": ["預言家", "女巫", "獵人"],
-      "mans": ["平民", "平民", "平民"]
+      "wolfsNum": 3,
+      "godsNum": 4,
+      "mansNum": 3,
+      "chracterText": "(狼王,2狼,預,女,獵,騎,3民)",
+      "characterAll": ["狼王", "狼人", "狼人", "預言家", "女巫", "獵人", "騎士", "平民", "平民", "平民"]
     },
   ]
 }
 
 let cate = modelData.cate
-let modelPlaying = {}
+let modelPlaying = {} // 所選模式
+let characterList = [] // 身分 & 順序
+let wolfsNum, godsNum, mansNum // 狼，神，民 - 數量
 
 // functions
+// *區間亂數
+const rand = (min, max) => {
+  let randArr = (window.crypto || window.msCrypto).getRandomValues(new Uint32Array(1));
+  let randNum = randArr[0] / (0xffffffff + 1)
+  return Math.floor((randNum * (max - min + 1) + min))
+}
+
+// *選擇模式
 const selectModel = (idx) => {
   console.log(idx)
   // 自訂模式
@@ -46,23 +60,60 @@ const selectModel = (idx) => {
   setting.classList.add("no-show") //關閉 setting
   setTimeout(() => {
     setting.classList.add("none")
+    giveCharacter.classList.remove("none")
   }, 250)
 
-  // 
+  // 給身分
+  give()
 }
 
-// *模式選擇
+// *給身分
+const give = () => {
+  let id, character, randNum, team
+  let allNum = modelPlaying.peopleNum - 1
+
+  // 狼神民 - 數量
+  wolfsNum = modelPlaying.wolfsNum
+  godsNum = modelPlaying.godsNum
+  mansNum = modelPlaying.mansNum
+
+  // 設定玩家初始值
+  for (let i = 0; i < modelPlaying.peopleNum; i++) {
+    id = i + 1
+    randNum = rand(0, allNum)
+    allNum--
+
+    // 隨機順序 & 身分 (轉移至新陣列)
+    character = modelPlaying.characterAll[randNum] // 身分
+    modelPlaying.characterAll.splice(randNum, 1)
+    // 團隊
+    character.includes("狼") ? team = "wolfs" : character.includes("民") ? team = "mans" : team = "gods"
+
+    characterList.push(
+      {
+        "id": id,
+        "character": character,
+        "team": team
+      }
+    )
+  }
+
+  console.log(modelPlaying.characterAll)
+  console.log(characterList)
+}
+
+// *click 模式選擇
 models.forEach((item, idx) => {
   // 插入 html
   item.innerHTML =
     `
       <span>
-        ${cate[idx].model}<span class="model-character">${cate[idx].chracterAll}</span>
+        ${cate[idx].model}<span class="model-character">${cate[idx].chracterText}</span>
       </span>
       <span class="model-num">${cate[idx].peopleNum}人</span>
     `
 
-  // 設定 model 點擊事件
+  // 設定 model 點擊事件 -> 確定模式
   item.addEventListener("click", () => {
     selectModel(idx)
   }, false)
